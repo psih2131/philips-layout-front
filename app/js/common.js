642e947603fa39a94e246031f9e3fd7f85261356
psih2131 <gearsdevelope@gmail.com>
2026-04-02 15:48:22 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,38 @@
+
+// $(function () {
+// 	$('.main-sec__slider').slick({
+// 		infinite: true,
+// 		speed: 1000,
+// 		adaptiveHeight: true,
+// 		slidesToScroll: 1,
+// 		dots: false,
+// 		slidesToShow: 1,
+// 		prevArrow: '<div class="ar_slier prev-ar_slide"><div class="ar-ic-slider"></div></div>',
+// 		nextArrow: '<div class="ar_slier next-ar_slide"><div class="ar-ic-slider"></div></div>',
+// 		responsive: [
+// 			{
+// 				breakpoint: 1200,
+// 				settings: {
+// 					autoplay: true,
+// 					autoplaySpeed: 3000,
+// 				}
+// 			},
+// 			{
+// 				breakpoint: 900,
+// 				settings: {
+// 					autoplay: true,
+// 					autoplaySpeed: 3000,
+// 					arrows: false,
+// 				}
+// 			},
+// 		]
+// 	});
+
+// });
+
+
+window.addEventListener('load', function () {
+
+}, false);
+
+
