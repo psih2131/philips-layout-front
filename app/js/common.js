887852408934d71af5acfd626b2f8f8fbd8886af
psih2131 <gearsdevelope@gmail.com>
2026-04-07 02:00:13 +0300

    fix

--- app/js/common.js
+++ app/js/common.js
@@ -6,6 +6,36 @@ $(function () {
         $(this).toggleClass('active');
     });
 
+    if (typeof Swiper !== 'undefined' && document.querySelector('.js-news-featured-swiper')) {
+        new Swiper('.js-news-featured-swiper', {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            speed: 450,
+            loop: true,
+            pagination: {
+                el: '.js-news-featured-pagination',
+                clickable: true,
+                bulletClass: 'news-intro-featured-bullet',
+                bulletActiveClass: 'news-intro-featured-bullet--active',
+            },
+        });
+    }
+
+    if (typeof Swiper !== 'undefined' && document.querySelector('.js-home-featured-swiper')) {
+        new Swiper('.js-home-featured-swiper', {
+            slidesPerView: 1,
+            spaceBetween: 0,
+            speed: 450,
+            loop: true,
+            pagination: {
+                el: '.js-home-featured-pagination',
+                clickable: true,
+                bulletClass: 'home-featured-bullet',
+                bulletActiveClass: 'home-featured-bullet--active',
+            },
+        });
+    }
+
 });
 
 
